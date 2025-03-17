6206cc89f8c6e0b0c16c20ae2ed189d460c5a1b2
MaximRyabtsev <rabcevmaksm@gmail.com>
2025-03-17 19:40:39 +0300

    Начинаем

---
+++ program/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_image/SDL_image.h>
+
+int main(){
+    std::cout << "Ура снова спп";
+}
